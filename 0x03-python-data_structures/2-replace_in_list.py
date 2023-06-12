f5648e919c1a416493066d5dc4d224777d08d550
Horma-j2 <ogundairoabiola5@gmail.com>
2023-06-12 16:10:13 -0700

    123

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or len(my_list) - 1 < idx:
-        return my_list
-    my_list[idx] = element
-    return
+    if (0 <= idx < len(my_list)):
+        my_list[idx] = element
+    return my_list
