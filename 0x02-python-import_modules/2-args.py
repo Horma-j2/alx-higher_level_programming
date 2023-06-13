f131e292fb5bf7cf8c63651cc4e5698a0543de46
Horma-j2 <ogundairoabiola5@gmail.com>
2023-06-13 14:18:06 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import calculator_1 as calc
-    calc.add.a = 10
-    calc.add.b = 5
+    calc.add.a = 10                                       calc.add.b = 5
     sm = calc.add(calc.add.a, calc.add.b)
     print("{} + {} = {}".format(calc.add.a, calc.add.b, sm))
 
@@ -12,11 +11,9 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(calc.sub.a, calc.sub.b, diff))
 
     calc.mul.a = 10
-    calc.mul.b = 5
-    prd = calc.mul(calc.mul.a, calc.mul.b)
+    calc.mul.b = 5                                        prd = calc.mul(calc.mul.a, calc.mul.b)
     print("{} * {} = {}".format(calc.mul.a, calc.mul.b, prd))
 
     calc.div.a = 10
-    calc.div.b = 5
-    qt = calc.div(calc.div.a, calc.div.b)
+    calc.div.b = 5                                        qt = calc.div(calc.div.a, calc.div.b)
     print("{} / {} = {}".format(calc.div.a, calc.div.b, qt))
