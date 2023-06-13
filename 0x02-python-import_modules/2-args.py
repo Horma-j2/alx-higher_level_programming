28ad1dc0f6f71cb4131b739c600435eac370b25c
Horma-j2 <ogundairoabiola5@gmail.com>
2023-06-13 14:26:00 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
+from sys import argv
+
+
+def principal():
+    print('{} argument'.format(len(argv) - 1), end='')
+    if len(argv) == 1:
+        print('s.')
+    elif len(argv) == 2:
+        print(':')
+    else:
+        print('s:')
+    for i in range(1, len(argv)):
+        print('{}: {}'.format(i, argv[i]))
+
+
 if __name__ == "__main__":
-    import calculator_1 as calc
-    calc.add.a = 10                                       calc.add.b = 5
-    sm = calc.add(calc.add.a, calc.add.b)
-    print("{} + {} = {}".format(calc.add.a, calc.add.b, sm))
-
-    calc.sub.a = 10
-    calc.sub.b = 5
-    diff = calc.sub(calc.sub.a, calc.sub.b)
-    print("{} - {} = {}".format(calc.sub.a, calc.sub.b, diff))
-
-    calc.mul.a = 10
-    calc.mul.b = 5                                        prd = calc.mul(calc.mul.a, calc.mul.b)
-    print("{} * {} = {}".format(calc.mul.a, calc.mul.b, prd))
-
-    calc.div.a = 10
-    calc.div.b = 5                                        qt = calc.div(calc.div.a, calc.div.b)
-    print("{} / {} = {}".format(calc.div.a, calc.div.b, qt))
+    principal()
