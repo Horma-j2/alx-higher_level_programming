8393450cb2beeef3e8a1ab5917c0024614a755b2
Horma-j2 <125504999+Horma-j2@users.noreply.github.com>
2023-08-10 09:35:21 +0100

    Delete 0x09-python-everything_is_object directory

--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Simple Class with specified fields"""
-
-
-class LockedClass:
-    """accepts only this field"""
-    __slots__ = {'first_name'}
--- 0x09-python-everything_is_object/19-copy_list.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def copy_list(lin):
-    return [x for x in lin]
